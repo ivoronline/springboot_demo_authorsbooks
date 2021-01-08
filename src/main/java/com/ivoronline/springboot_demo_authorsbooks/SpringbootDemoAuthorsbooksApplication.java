30bc0f3c46d47cf71ad7106d47ac227bbdde998d
igoreta <ivoronline@gmail.com>
2021-01-08 08:32:19 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_demo_authorsbooks/SpringbootDemoAuthorsbooksApplication.java
@@ -0,0 +1,15 @@
+package com.ivoronline.springboot_demo_authorsbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoAuthorsbooksApplication {
+
+  public static void main(String[] args) {
+    System.out.println("Hello");
+    SpringApplication.run(SpringbootDemoAuthorsbooksApplication.class, args);
+  }
+
+}
+
---
+++ src/test/java/com/ivoronline/springboot_demo_authorsbooks/SpringbootDemoAuthorsbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_demo_authorsbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoAuthorsbooksApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
