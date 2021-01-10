b127a7ed2838c08bb386429abec0c853211e4063
igoreta <ivoronline@gmail.com>
2021-01-10 16:50:41 +0100

    Initial Commit

---
+++ src/main/java/com/ivoronline/springboot_demo_authorsbooks/controllers/ComboController.java
@@ -0,0 +1,54 @@
+package com.ivoronline.springboot_demo_authorsbooks.controllers;
+
+import com.ivoronline.springboot_demo_authorsbooks.entities.Author;
+import com.ivoronline.springboot_demo_authorsbooks.entities.Book;
+import com.ivoronline.springboot_demo_authorsbooks.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+import java.util.Set;
+
+@Controller
+public class ComboController {
+
+  //======================================================================
+  // REPOSITORIES
+  //======================================================================
+  @Autowired AuthorRepository authorRepository;
+
+  //======================================================================
+  // METHOD: GET BOOKS FORM
+  //======================================================================
+  @RequestMapping("/GetBooksForm")
+  public String getBooksForm() {
+    return "GetBooksForm";
+  }
+
+  //======================================================================
+  // METHOD: GET BOOKS
+  //======================================================================
+  @ResponseBody
+  @RequestMapping("/GetBooks")
+  public String getBooks(@RequestParam Integer authorId) {
+
+    //GET AUTHOR & BOOKS
+    Optional<Author> authorOptional = authorRepository.findById(authorId);
+    Author           author         = authorOptional.get();
+    Set<Book>        books          = author.getBooks();
+
+    //DISPLAY BOOKS
+    String result = "<h2> Books by " + author.getName() + "</h2>";
+    for(Book book : books) {
+      result += book.getTitle() + "<br>";
+    }
+
+    //RETURN
+    return result;
+
+  }
+
+}
